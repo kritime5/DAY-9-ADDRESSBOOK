868437273ac5795e8bf9f800a4acd259791bb5aa
Kritika <kritsme963@gmail.com>
2022-11-03 10:20:00 -0700

    Added:create contact in address book

--- src/com/bridgelabz/oops/AddressBook.java
+++ src/com/bridgelabz/oops/AddressBook.java
@@ -1,8 +1,18 @@
 package com.bridgelabz.oops;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class AddressBook{
+    //UC1-CREATE A CONTACT IN ADDRESS BOOK
+    Scanner s=new Scanner(System.in);
+    List<Contacts> contactsArrayList=new ArrayList<Contacts>();
     public static void main(String[] args) {
+
         System.out.println("Welcome to Address book program");
+
     }
 
 }
---
+++ src/com/bridgelabz/oops/Contacts.java
@@ -0,0 +1,90 @@
+package com.bridgelabz.oops;
+
+public class Contacts {
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    String zipcode;
+    String phoneNumber;
+    String emailId;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    @Override
+    public String toString() {
+        return "Contacts{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zipcode='" + zipcode + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", emailId='" + emailId + '\'' +
+                '}';
+    }
+}
